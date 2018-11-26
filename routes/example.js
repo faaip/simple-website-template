2dbe826cc2b36fd876b3b1fc564885d762171b6f
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2018-11-26 16:29:26 +0100

    removed contentful and added sensible inline  comments

--- routes/example.js
+++ routes/example.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var client = require('../services/contentfulClient').client;
 
 // http://localhost:7000/example
-router.get('/', function(req, res, next) {
-    client.getEntries()
-    .then(function (entries) {
-        res.render('pages/example', {entries: entries.items});
-    }).catch(function(error) {
-        if(error.response.status === 404) {
-            return next(error.response);
-        }
-        else {
-            res.render('pages/example', {});
-        }
-    });
+router.get('/', function (req, res, next) {
+    if (error.response.status === 404) {
+        return next(error.response);
+    }
 });
 
 module.exports = router;
